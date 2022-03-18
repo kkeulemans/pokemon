7fd4a01de383fcb1a0253799d03b4d2043669c53
kkeulemans <78099351+kkeulemans@users.noreply.github.com>
2022-03-18 13:08:38 +0100

    hgsd

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import axios from "axios";
 import Card from "./components/Card";
-
+import logo from "./assets/1200px-International_Pokémon_logo.svg.png";
 function App() {
     const [pokemons, setPokemons] = useState([]);
     const [endpoint, setEndpoint] = useState("https://pokeapi.co/api/v2/pokemon")
@@ -11,33 +11,32 @@ function App() {
             try {
                 const result = await axios.get(`${endpoint}`);
                 setPokemons(result.data);
-
-
             } catch (e) {
                 console.error("NOT FOUND");
             }
-
         }
-
         fetchData();
-
 
     },);
 
     return (
-        <div>
-
-            {Object.keys(pokemons).length > 0 &&
+        <span>
+            <img src={logo}/>
+            <p></p>
+            <button type="button" onClick={() => setEndpoint(pokemons.previous)}
+                disabled={pokemons.previous === null}>Previous
+        </button>
+    <button type="button" onClick={() => setEndpoint(pokemons.next)}>Next</button>
+            <div>
+                {Object.keys(pokemons).length > 0 &&
                 pokemons.results.map((pokemon) => {
                     return <Card key={pokemon.name} type={pokemon.name}/>
                 })
             }
 
-            <button type="button" onClick={() => setEndpoint(pokemons.previous)}
-                    disabled={pokemons.previous === null}>Previous
-            </button>
-            <button type="button" onClick={() => setEndpoint(pokemons.next)}>Next</button>
+
         </div>
+            </span>
     );
 }
 
